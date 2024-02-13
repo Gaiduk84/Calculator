771c782165e621a6776c39fbcfd7693f1e03b7ca
Gaiduk84 <151941754+Gaiduk84@users.noreply.github.com>
2024-02-13 20:00:51 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,52 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+    public static String calc(String input) {
+        String[] tokens = input.trim().split(" ");
+        if (tokens.length != 3) {
+            System.out.println("throws Exception");
+            throw new IllegalArgumentException("Введите два числа");
         }
+
+        int a = 0;
+        int b = 0;
+        try {
+            a = Integer.parseInt(tokens[0]);
+            b = Integer.parseInt(tokens[2]);
+        } catch (NumberFormatException e) {
+            System.out.println("throws Exception");
+            throw new IllegalArgumentException("Только арабские числа ");
+        }
+        if (a >= 11 || a <= 0||b > 11 || b <= 0) {
+            System.out.println("throws Exception");
+            throw new IllegalArgumentException(" Числа должны быть от 1 до 10");
+        }
+        int result = 0;
+        switch (tokens[1]) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            default:
+                System.out.println("throws Exception");
+                throw new IllegalArgumentException("throws Exception");
+        }
+
+        return Integer.toString(result);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Введите выражение через пробел");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String result = calc(input);
+        System.out.println(result);
     }
 }
